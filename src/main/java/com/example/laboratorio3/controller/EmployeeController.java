fcd5279aa3a8b64d7a1c3de8d43e2b049f16afed
Arkel <144744565+WalkedPlate@users.noreply.github.com>
2024-04-19 20:44:26 -0500

    lista inicial employee

--- src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -1,32 +1,54 @@
 package com.example.laboratorio3.controller;
 
-//COMPLETAR
+import com.example.laboratorio3.entity.Employees;
+import com.example.laboratorio3.repository.EmployeesRepository;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
 public class EmployeeController {
 
-    //COMPLETAR
+    final EmployeesRepository employeesRepository;
 
-    public String listaEmployee(   ){
-        //COMPLETAR
+
+    public EmployeeController(EmployeesRepository employeesRepository){
+        this.employeesRepository = employeesRepository;
+
+    }
+    @GetMapping(value ={"/employee","","/"})
+
+    public String listaEmployee(Model model){
+
+        List<Employees> lista =employeesRepository.findAll();
+        model.addAttribute("lista",lista);
+
+        return "employee/lista";
     }
 
     public String nuevoEmployeeForm( ) {
-        //COMPLETAR
+        return "employee/lista";
     }
 
 
+    @PostMapping
     public String guardarEmployee() {
-        //COMPLETAR
+        return "redirect:/employee";
     }
 
-
+    @GetMapping
     public String editarEmployee() {
-        //COMPLETAR
+        return "employee/lista";
     }
 
 
     public String borrarEmpleado() {
 
-       //COMPLETAR
+        return "employee/lista";
 
     }
 
--- src/main/java/com/example/laboratorio3/entity/Employees.java
+++ src/main/java/com/example/laboratorio3/entity/Employees.java
@@ -1,12 +1,51 @@
 package com.example.laboratorio3.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.*;
 import java.util.Date;
-
-//COMPLETAR
+@Getter
+@Setter
+@Entity
+@Table(name = "employees")
 public class Employees {
 
-           //COMPLETAR
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name="employee_id",nullable = false)
+    private int id;
+
+    @Column(name="first_name",nullable = true)
+    private String firstName;
+
+    @Column(name="last_name",nullable = true)
+    private String lastName;
+
+    @Column(name="email",nullable = true)
+    private String email;
+
+    @Column(name="phone_number",nullable = true)
+    private String phoneNumber;
+
+    @Column(name="hire_date",nullable = true)
+    private String hireDate;
+
+    @Column(name="job_id",nullable = true)
+    private String jobId;
+
+    @Column(name="salary",nullable = true)
+    private Float salary;
+
+    @Column(name="commission_pct",nullable = true)
+    private Float Float;
+
+    @Column(name="manager_id",nullable = true)
+    private Integer managerId;
+
+    @Column(name="department_id",nullable = true)
+    private Integer departmentId;
+
 }
