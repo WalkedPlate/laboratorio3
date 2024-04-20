9677716350efa6081c51469de616eba93ddcac09
Diego Henriquez <a20182044@pucp.edu.pe>
2024-04-19 20:43:46 -0500

    readme

--- src/main/java/com/example/laboratorio3/controller/HistoryController.java
+++ src/main/java/com/example/laboratorio3/controller/HistoryController.java
@@ -1,9 +1,14 @@
 package com.example.laboratorio3.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 //COMPLETAR
+@Controller
 public class HistoryController {
-//COMPLETAR
-
-
+    @GetMapping(value = "/historial")
+    public String paginaHistorial(){
+        return ;
+    }
 }
